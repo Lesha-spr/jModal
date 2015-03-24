4c7489be4ece8bb33b8255263fc910ffd7e4bbd5
Lesha-spr <voodoo.spr@gmail.com>
2015-03-24 23:24:39 +0200

    jModal: work with todos, added more examples

--- jModal.js
+++ jModal.js
@@ -1,177 +1,180 @@
 (function (window, console, document, $) {
-	'use strict';
+    'use strict';
 
-	if ($.fn.jModal) {
-		return console.log('jModal already included');
-	}
+    if ($.fn.jModal) {
+        return console.log('jModal already included');
+    }
 
-	/**
-	 *
-	 * @param element {Element}
-	 * @param options {Object}
-	 * @constructor
-	 */
+    /**
+     *
+     * @param element {Element}
+     * @param actions {Object}
+     * @constructor
+     */
 
-	var Modal = function Modal(element, options) {
-		var bPassedOptions = options && !$.isEmptyObject(options),
-			sClose;
+    var Modal = function Modal(element, actions) {
+        var bPassedOptions = actions && !$.isEmptyObject(actions),
+            sClose;
 
-		this.hParam = bPassedOptions ? $.extend({}, $.fn.jModal.defaults, options) : $.fn.jModal.defaults;
+        this.hParam = bPassedOptions ? $.extend({}, $.fn.jModal.defaults, actions) : $.fn.jModal.defaults;
 
-		sClose = typeof this.hParam.closeText === 'string' ? this.hParam.closeText : $.fn.jModal.defaults.closeText;
+        sClose = typeof this.hParam.closeText === 'string' ? this.hParam.closeText : $.fn.jModal.defaults.closeText;
 
-		this.hElements = {
-			jElement: $(element),
-			jDocument: $(document),
-			jBody: $('body'),
-			jModal: $(this.hParam.modal).addClass(this.hParam.classes.modal),
-			jClose: $(this.hParam.close).addClass(this.hParam.classes.close).text(sClose)
-		};
+        this.hElements = {
+            jElement: $(element),
+            jDocument: $(document),
+            jBody: $('body'),
+            jModal: $(this.hParam.modal).addClass(this.hParam.classes.modal),
+            jClose: $(this.hParam.close).addClass(this.hParam.classes.close).text(sClose)
+        };
 
-		this.hElements.jContent = this.hElements.jModal.children();
+        this.hElements.jContent = this.hElements.jModal.children();
+    
+        this.bOpen = false;
+    
+        this._initialize();
+        this._attachEvents();
+    };
 
-		this.bOpen = false;
+    $.extend(Modal.prototype, {
+        _initialize: function _initialize() {
+            if (!this.hParam.ajax) {
+                this.set(this.hParam.html);
+            }
 
-		this._initialize();
-		this._attachEvents();
-	};
+            return this;
+        },
 
-	$.extend(Modal.prototype, {
-		_initialize: function _initialize() {
-			if (!this.hParam.ajax) {
-				this.set(this.hParam.html);
-			}
+        set: function append(html) {
+            this.hElements.jContent
+                .addClass(this.hParam.classes.inner)
+                .html(this.hParam.prepare(html))
+                .append(this.hElements.jClose);
+        },
 
-			return this;
-		},
+        _attachEvents: function _attachEvents() {
+            this.hElements.jElement.on(this.hParam.events.open, $.proxy(this.openModal, this));
+            this.hElements.jModal.add(this.hElements.jClose)
+                .on(this.hParam.events.close, $.proxy(this.closeModal, this));
+            this.hElements.jDocument.on('keyup', $.proxy(this._keyClose, this));
+            this.hElements.jContent.on(this.hParam.event, function(event) {
+                event.stopPropagation();
+            });
+        },
 
-		set: function append(html) {
-			this.hElements.jContent
-				.addClass(this.hParam.classes.inner)
-				.html(this.hParam.prepare(html))
-				.append(this.hElements.jClose);
-		},
+        openModal: function openModal(event) {
+            var _this = this;
 
-		_attachEvents: function _attachEvents() {
-			this.hElements.jElement.on(this.hParam.events.open, $.proxy(this.openModal, this));
-			this.hElements.jModal.add(this.hElements.jClose)
-				.on(this.hParam.events.close, $.proxy(this.closeModal, this));
-			this.hElements.jDocument.on('keyup', $.proxy(this._keyClose, this));
-			this.hElements.jContent.on(this.hParam.event, function(event) {
-				event.stopPropagation();
-			});
-		},
+            if (event) {
+                event.preventDefault();
+            }
 
-		openModal: function openModal(event) {
-			var _this = this;
+            // TODO: wait for any async function
+            if (_this.hParam.ajax) {
+                $.ajax($.extend({}, _this.hParam.ajaxSettings, _this.hParam.ajax, {
+                    success: function(data) {
+                        _this.set(data);
+                        _this.showModal();
 
-			if (event) {
-				event.preventDefault();
-			}
+                        if (_this.hParam.ajax.success) {
+                            _this.hParam.ajax.success.apply(this, arguments);
+                        }
+                    }
+                }));
 
-			// TODO: wait for any async function
-			if (_this.hParam.ajax) {
-				$.ajax($.extend({}, _this.hParam.ajaxSettings, _this.hParam.ajax, {
-					success: function(data) {
-						_this.set(data);
-						_this.showModal();
+            } else if (_this.hParam.promise && _this.hParam.promise.done) {
+                _this.hParam.promise.done(function(data) {
+                    _this.set(data);
+                    _this.showModal();
 
-						// TODO: remove check for callback passed
-						if (_this.hParam.ajax.success) {
-							_this.hParam.ajax.success.apply(this, arguments);
-						}
-					},
-					error: function(jqXHR, textStatus, errorThrown) {
-						// TODO: remove check for callback passed
-						if (_this.hParam.ajax.error) {
-							_this.hParam.ajax.error.apply(this, arguments);
-						}
-					}
-				}));
-			} else {
-				_this.showModal();
-			}
-		},
+                    return this;
+                });
 
-		showModal: function showModal() {
-			this.hElements.jBody.append(this.hElements.jModal);
-			this.bOpen = true;
+            } else {
+                _this.showModal();
+            }
+        },
 
-			this.hParam.onOpen(this);
-		},
+        showModal: function showModal() {
+            this.hElements.jBody.append(this.hElements.jModal);
+            this.bOpen = true;
 
-		closeModal: function closeModal(event) {
-			if (event) {
-				event.preventDefault();
-			}
+            this.hParam.onOpen(this);
+        },
 
-			if (this.bOpen) {
-				this.hElements.jModal.detach();
-				this.bOpen = false;
+        closeModal: function closeModal(event) {
+            if (event) {
+                event.preventDefault();
+            }
 
-				this.hParam.onClose(this);
-			}
-		},
+            if (this.bOpen) {
+                this.hElements.jModal.detach();
+                this.bOpen = false;
 
-		// Handle 'esc' key
-		_keyClose: function _keyClose(event) {
-			if (event && event.keyCode === 27) {
-				this.closeModal();
-			}
-		}
-	});
+                this.hParam.onClose(this);
+            }
+        },
 
-	$.fn.jModal = function (option, global) {
-		var args = arguments;
+        // Handle 'esc' key
+        _keyClose: function _keyClose(event) {
+            if (event && event.keyCode === 27) {
+                this.closeModal();
+            }
+        }
+    });
 
-		return this.each(function () {
-			if (!$.data(this, 'jModal')) {
-				$.data(this, 'jModal', new Modal(this, option));
-			} else {
-				var jModal = $(this);
+    $.fn.jModal = function (action, params) {
+        var args = arguments;
 
-				if (args.length) {
-					if (option) {
-						// TODO: extend to getters
-						jModal.data('jModal')[option](global);
-					} else {
-						$.error('No such method in jModal plugin')
-					}
-				}
+        return this.each(function () {
+            if (!$.data(this, 'jModal')) {
+                $.data(this, 'jModal', new Modal(this, action));
 
-				return jModal;
-			}
+            } else {
+                var jModal = $(this);
 
-			return this;
-		});
-	};
+                if (args.length) {
+                    if (typeof jModal.data('jModal')[action] === 'function') {
+                        // TODO: extend to getters
+                        jModal.data('jModal')[action](params);
+                    } else {
+                        $.error('No such method in jModal plugin')
+                    }
+                }
 
-	$.fn.jModal.defaults = {
-		classes: {
-			modal: 'g-modal',
-			inner: 'g-modal__inner',
-			close: 'g-modal__inner__close'
-		},
-		events: {
-			open: 'click',
-			close: 'click'
-		},
-		modal: '<div><div></div></div>',
-		close: '<a href="#"/>',
-		closeText: 'Close',
-		beforeSet: $.noop,
-		prepare: function(html) {
-			return html;
-		},
-		onOpen: $.noop,
-		onClose: $.noop,
-		ajaxSettings: {
-			cache: false
-		}
-	};
+                return jModal;
+            }
 
-	$.fn.jModal.setDefaults = function(hDefaults) {
-		$.extend($.fn.jModal.defaults, hDefaults);
-	};
+            return this;
+        });
+    };
+
+    $.fn.jModal.defaults = {
+        classes: {
+            modal: 'g-modal',
+            inner: 'g-modal__inner',
+            close: 'g-modal__inner__close'
+        },
+        events: {
+            open: 'click',
+            close: 'click'
+        },
+        modal: '<div><div></div></div>',
+        close: '<a href="#"/>',
+        closeText: 'Close',
+        beforeSet: $.noop,
+        prepare: function(html) {
+            return html;
+        },
+        onOpen: $.noop,
+        onClose: $.noop,
+        ajaxSettings: {
+            cache: false
+        }
+    };
+
+    $.fn.jModal.setDefaults = function(hDefaults) {
+        $.extend($.fn.jModal.defaults, hDefaults);
+    };
 
 })(window, window.console, document, window.jQuery);
